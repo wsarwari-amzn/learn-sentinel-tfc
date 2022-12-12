951e51d04027f93ab2aa59dc80b2d5470921b19c
Waleed Sarwari <sarwaw@amazon.com>
2022-12-12 10:49:25 -0500

    org

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "waleeds-sandbox"
 
     workspaces {
       name = "sentinel-example"
